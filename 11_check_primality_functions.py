cf999cb5feea2583ff4c9b9c87b14d24a19fd882
Andres Borda <aborda38@outlook.com>
2021-01-24 10:18:23 -0500

    Uploading exercise 11

---
+++ 11_check_primality_functions.py
@@ -0,0 +1,20 @@
+#Ask the user for a number and determine whether the number is prime or not.
+
+def run ():
+    def numprime():
+        user_number = int(input('Type a number to check primality --> '))
+        contador = 0
+        for i in range (1, user_number + 1):
+            if user_number % i == 0:
+                contador = contador + 1
+  
+        if contador == 2:
+            print ("***", user_number, "--> is a prime number, XD")
+        else:
+            print ("***", user_number, "--> isn't a prime number, :(")
+    #Function end.
+
+    numprime()
+
+if __name__ == '__main__':
+    run()
